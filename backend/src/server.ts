3c6564b2e04d798deb4046d3749cfa9c5266d1b1
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-02-27 15:50:27 -0300

    refactor: clean code, indian use .then, i am no

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,16 +8,25 @@ const port = Number(config.port)
 console.log("PORT:", config.port)
 console.log("HOST:", config.host)
 
-const handler = (_req: IncomingMessage, res: ServerResponse) => {
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", config.CORS_ORIGIN!);
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    // router(req,rep) WIP
+
+async function main() {
+    try {
+        const handler = (_req: IncomingMessage, res: ServerResponse) => {
+            res.setHeader("Content-Type", "application/json");
+            res.setHeader("Access-Control-Allow-Origin", config.CORS_ORIGIN!);
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+            // router(req,rep) WIP
+        }
+
+        const server = new HTTPServer(port, host, handler)
+
+        await server.StartServer()
+        console.log(`Server is listening on http://${host}:${port}`)
+
+    } catch (e) {
+        console.log(e)
+    }
 }
 
-const server = new HTTPServer(port, host, handler)
-
-server.StartServer().then(() => [
-    console.log(`Server is listening on http://${host}:${port}`)
-])
+await main();
